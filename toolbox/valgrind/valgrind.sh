3a3582151e95c66f42a6170a02be6ebd39665ac3
Anthony Tsang <anthonytsang0226@gmail.com>
2022-11-27 05:48:38 +0100

    added error handling for valgrind commands

--- toolbox/valgrind/valgrind.sh
+++ toolbox/valgrind/valgrind.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 valgrind() {
-  if [ "$1" = "-h" ] || [ "$1" = "--help" ]
-  then
+  if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     echo -e $BLUE"
 Usage:
 
@@ -10,15 +9,24 @@ valgrind <valgrind_arguments>  Run valgrind directly
 "$WHITE
     return
   fi
-  local current_path=$(pwd | sed "s,$WORKSPACE42,,g")
+  local current_path=$(pwd | sed "s,^$WORKSPACE42,,g")
+  if [ "$current_path" = $(pwd) ]; then
+    echo -e $RED"You are not inside the workspace specified."$WHITE
+    echo -e $BLUE"Valgrind can only be ran inside the specified workspace, currently it is set to \"$WORKSPACE42\". To change it, please change it inside \"$(dirname "$(type open-docker | awk '{ print $7 }')")/global\"."$WHITE
+    return
+  fi
+  if [[ -z "$(! docker stats --no-stream 2> /dev/null)" ]]; then
+    echo -e $RED"Docker is not running"$WHITE
+    echo -e $BLUE"Please run init-valgrind to initiate valgrind"$WHITE
+    return
+  fi
   local cmd=$(echo "${@:0}" | sed "s,\./,/data$current_path/,g")
   echo $GREEN"Running valgrind $cmd..."$WHITE
   /usr/local/bin/docker exec -it valgrind bash -c "$cmd"
 }
 
 valgrind-cli() {
-  if [ "$1" = "-h" ] || [ "$1" = "--help" ]
-  then
+  if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     echo -e $BLUE"
 Usage:
 
@@ -26,7 +34,17 @@ valgrind-cli <comannds>        Execute any command inside docker container witho
 "$WHITE
     return
   fi
-  local current_path=$(pwd | sed "s,$WORKSPACE42,,g")
+  local current_path=$(pwd | sed "s,^$WORKSPACE42,,g")
+  if [ "$current_path" = $(pwd) ]; then
+    echo -e $RED"You are not inside the workspace specified."$WHITE
+    echo -e $BLUE"Valgrind can only be ran inside the specified workspace, currently it is set to \"$WORKSPACE42\". To change it, please change it inside \"$(dirname "$(type open-docker | awk '{ print $7 }')")/global\"."$WHITE
+    return
+  fi
+  if [[ -z "$(! docker stats --no-stream 2> /dev/null)" ]]; then
+    echo -e $RED"Docker is not running"$WHITE
+    echo -e $BLUE"Please run init-valgrind to initiate valgrind"$WHITE
+    return
+  fi
   /usr/local/bin/docker exec -it valgrind bash -c "cd /data$current_path && ${@:1}"
 }
 
@@ -36,7 +54,7 @@ init-valgrind() {
     if [ "$(docker ps -aq -f status=exited -f name=valgrind)" ]; then
       /usr/local/bin/docker start valgrind
     else
-      local dockerfile=$(dirname "$(type $0 | awk '{ print $7 }')")/valgrindDockerfile
+      local dockerfile=$(dirname "$(type $0 | awk '{ print $7 }')")/Dockerfile
       chmod 755 $dockerfile
       /usr/local/bin/docker build . -t valgrind -f $dockerfile &&
       /usr/local/bin/docker run -itd -v $WORKSPACE42:/data --name=valgrind valgrind
